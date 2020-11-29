8423300a2776c67e92d0a80541a72199ee73fa24
Rando T <rt.peakonto@gmail.com>
2020-11-29 13:54:47 +0200

    Lisa päise kaunistus.

---
+++ joonistamine.js
@@ -0,0 +1,41 @@
+
+let aeg = 0;
+
+function joonista() {
+    var c = document.getElementById("taust");
+    var ctx = c.getContext("2d");
+    const { width, height } = c.getBoundingClientRect();
+    c.width = width;
+    c.height = height; 
+    //console.log(width, height)
+    ctx.scale(width/100, height/100);
+    t=aeg/2;
+    console.log(t)
+
+    ctx.fillStyle = '#ff0000';
+    ctx.fillRect(0, 0, 100, 100);
+    s = 1;
+    n =50;
+    for (const xx of [(x)=>50+x+10, (x)=>50-x-10]) {
+        for (let i = 0; i < n; i++) {
+            //if (i!=t%10) {continue;}
+            x = (x) => xx((i+x*2*(1+Math.sin(t/300)*5)) * 100/n);
+            f = Math.sin(t/50)*10*s;
+            ctx.beginPath();
+            ctx.moveTo(x(1), 50);
+            ctx.lineTo(x(0.5), 80+f);
+            ctx.lineTo(x(0), 50);
+            ctx.lineTo(x(0.5), 20+f);
+            ctx.closePath();
+            ctx.stroke();
+            s = - s;    
+        }
+    }
+    
+    aeg++;
+}
+
+window.onload = () => {
+    console.log('UU')
+    window.setInterval(joonista, 100)
+};
